a7c60d4f67a2de220cfa178f99798b1d62d09b3d
Christopher Bailey <cbailey@mort.is>
2017-11-19 23:14:33 -0500

    Removes microsoft_auth.apps and adds djangoql to test/dev requirements

--- microsoft_auth/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LiveConnectConfig(AppConfig):
-    name = 'microsoft_auth'
--- tests/settings.py
+++ tests/settings.py
@@ -14,6 +14,7 @@ INSTALLED_APPS = [
 
     'microsoft_auth',
     'tests',
+    'djangoql',  # optional
 ]
 
 DATABASES = {
