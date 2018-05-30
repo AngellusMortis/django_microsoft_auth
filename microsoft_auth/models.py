706565edd0049841ba16640980b25f88a234e806
Landelin DELAGARDE <landelagarde@gmail.com>
2018-05-30 15:38:53 +0200

    Fixes max length o365 ID
    Increase the max length o365 ID to 36 characters

--- microsoft_auth/models.py
+++ microsoft_auth/models.py
@@ -20,7 +20,7 @@ for field in User._meta.fields:
 
 
 class MicrosoftAccount(models.Model):
-    microsoft_id = models.CharField(_('microsoft account id'), max_length=32)
+    microsoft_id = models.CharField(_('microsoft account id'), max_length=36)
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE, null=True,
