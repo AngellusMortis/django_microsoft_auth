b48b9203bac3e56ff9ae93fae913fdf9919c1aea
Christopher Bailey <cbailey@mort.is>
2019-03-26 22:37:25 -0400

    Adds example login form. Renames login JS/CSS

--- tests/site/urls.py
+++ tests/site/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r"^accounts/", include("django.contrib.auth.urls")),
     url(r"^admin/", admin.site.urls),
     url(
         r"^microsoft/",
