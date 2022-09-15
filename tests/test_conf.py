0bb38fa54214d4da57ae39d3e6f8734bec45975b
7oi <7oi@7oi.is>
2022-09-15 11:27:36 +0000

    Assert that conf class was instantiated in init_config method

--- tests/test_conf.py
+++ tests/test_conf.py
@@ -3,6 +3,7 @@
 """
 
 from django.test import override_settings
+from unittest.mock import patch
 
 from microsoft_auth.conf import DEFAULT_CONFIG, SimpleConfig
 
@@ -68,9 +69,13 @@ class ConfTests(TransactionTestCase):
     @override_settings(MICROSOFT_AUTH_CONFIG_CLASS="tests.test_conf.SimpleTestConfig")
     def test_custom_config_class_uninstantiated(self):
         """Tests MICROSOFT_AUTH_CONFIG_CLASS set to another class (uninstantiated)"""
-        from microsoft_auth.conf import config
+        from microsoft_auth.conf import config, init_config
 
         self.assertTrue(isinstance(config, SimpleTestConfig))
+
+        with patch("tests.test_conf.SimpleTestConfig") as mockClass:
+            init_config()
+            mockClass.assert_called_once()
 
     @override_settings(
         MICROSOFT_AUTH_CONFIG_CLASS="tests.test_conf.SimpleTestNoDefaultConfig"
@@ -78,6 +83,10 @@ class ConfTests(TransactionTestCase):
     def test_custom_config_class_with_no_default_uninstantiated(self):
         """Tests MICROSOFT_AUTH_CONFIG_CLASS set to another class (uninstantiated) with no
         add_default_config option"""
-        from microsoft_auth.conf import config
+        from microsoft_auth.conf import config, init_config
 
         self.assertTrue(isinstance(config, SimpleTestNoDefaultConfig))
+
+        with patch("tests.test_conf.SimpleTestNoDefaultConfig") as mockClass:
+            init_config()
+            mockClass.assert_called_once()
